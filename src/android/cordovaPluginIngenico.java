30495d251d55e882178c5a173802575013071c32
Mayank Patel <mayank@webfirminfotech.com>
2017-10-25 16:11:04 +0530

    change

--- src/android/cordovaPluginIngenico.java
+++ src/android/cordovaPluginIngenico.java
@@ -7,6 +7,10 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.ingenico.rba_sdk.*;
+import com.ingenico.rba_sdk.RBA_API.LOG_LEVEL;
+import com.ingenico.rba_sdk.RBA_API.LOG_OUTPUT_FORMAT_OPTIONS;
+
 /**
  * This class echoes a string called from JavaScript.
  */
@@ -23,10 +27,23 @@ public class cordovaPluginIngenico extends CordovaPlugin {
     }
 
     private void coolMethod(String message, CallbackContext callbackContext) {
+        RBA_API.SetDefaultLogLevel(LOG_LEVEL.LTL_TRACE);
+        RBA_API.SetTraceOutputFormatOption(LOG_OUTPUT_FORMAT_OPTIONS.LOFO_NO_INSTANCE_ID);
+        RBA_API.SetTraceOutputFormatOption(LOG_OUTPUT_FORMAT_OPTIONS.LOFO_NO_THREAD_ID);
+        RBA_API.SetLogCallBack(new LogTrace());
+        RBA_API.Initialize();
         if (message != null && message.length() > 0) {
             callbackContext.success(message);
         } else {
             callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+
+    private class LogTrace implements LogTraceInterface
+    {
+        public void Log(String  logLine)
+        {
+            System.out.println(logLine);
         }
     }
 }
