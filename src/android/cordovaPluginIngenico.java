9bcb4df3d76a5df9c68a8b40b0e6cfdd332664ae
Mayank Patel <mayank@webfirminfotech.com>
2017-10-26 13:06:41 +0530

    SSL certy Added

--- src/android/cordovaPluginIngenico.java
+++ src/android/cordovaPluginIngenico.java
@@ -1,11 +1,16 @@
 package cordova.plugin.ingenico;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import android.content.Context;
 
 import com.ingenico.rba_sdk.*;
 import com.ingenico.rba_sdk.RBA_API.LOG_LEVEL;
@@ -17,9 +22,12 @@ import com.ingenico.rbasdk_android_adapter.*;
  */
 public class cordovaPluginIngenico extends CordovaPlugin {
 
+    private Context context;
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("connect")) {            
+        context = this.cordova.getActivity().getApplicationContext();
+        if (action.equals("connect")) {
             this.connect(args.getString(0), args.getString(1),callbackContext);
             return true;
         }
@@ -29,22 +37,53 @@ public class cordovaPluginIngenico extends CordovaPlugin {
     private void connect(String ip_address,String port, CallbackContext callbackContext) {
         if(ip_address.trim() != null && ip_address.trim().length() > 0 && port.trim() != null && port.trim().length() > 0 ){
             if(this.RBA_SDK_Initialize()){
+                InputStream clientCertKeyFile = null;
+                InputStream caCertFile        = null;
+
                 Comm_Settings comSettings = new Comm_Settings();
+                RBASDKAdapter.SetApplicationContext(context);
                 comSettings.Interface_id      = Comm_Settings_Constants.SSL_TCPIP_INTERFACE;
                 comSettings.IP_Address   = ip_address;
                 comSettings.Port_Num     = port;
+                System.out.println(comSettings.IP_Address);
+                System.out.println(comSettings.Port_Num);
+
+                try{ caCertFile   		= context.getAssets().open("CA_SERVER_CERT.PEM");
+                System.out.println(caCertFile);
+                System.out.println("get caCertFile assets");
+                }
+                catch  (IOException e) {
+                  System.out.println("IO exception:"+e.toString());
+                   }
+
+                try{ clientCertKeyFile   = context.getAssets().open("CLIENT.bks");
+                System.out.println(clientCertKeyFile);
+                System.out.println("get clientCertKeyFile assets");}
+                catch  (IOException e) {
+                  System.out.println("IO exception:"+e.toString());
+                  try{ caCertFile.close();}catch(IOException ee){
+                    System.out.println("IO exception:"+ ee.toString());
+                  }
+                  return;
+                }
+
+                char keystorepass[]	= "Ingenico".toCharArray();
+                RBASDKAdapter.SetCaCerticicate(caCertFile);
+                RBASDKAdapter.SetLocalCerticicate(clientCertKeyFile,keystorepass);
+
                 ERROR_ID ret = RBA_API.Connect(comSettings);
+                System.out.println(ret);
                 if (ret == ERROR_ID.RESULT_SUCCESS){
                     callbackContext.success("connected");
                 }else{
-                    callbackContext.success("initialized");                
+                    callbackContext.success("initialized");
                 }
             } else {
                 callbackContext.error("Expected one non-empty string argument.");
-            }   
+            }
         } else {
             callbackContext.error("Ip Address and port is missing");
-        }        
+        }
     }
 
     private boolean RBA_SDK_Initialize()
