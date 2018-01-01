0ebeae8a99329b5749b2361a898568370534a4a5
Mayank Patel <mayank@webfirminfotech.com>
2018-01-01 16:17:20 +0530

    change

--- src/android/cordovaPluginIngenico.java
+++ src/android/cordovaPluginIngenico.java
@@ -149,7 +149,9 @@ public class cordovaPluginIngenico extends CordovaPlugin {
     }
   }
 
-  private void voidTransation(String ip_address, String port, String refNo, CallbackContext callbackContext) {
+  private void voidTransation(String ip_address, String port, String refNo, CallbackContext callbackContext)
+      throws JSONException {
+    JSONObject JSONResponse = new JSONObject();
     if (ip_address.trim() != null && ip_address.trim().length() > 0 && port.trim() != null && port.trim().length() > 0
         && refNo.trim() != null && refNo.trim().length() > 0) {
 
@@ -229,57 +231,57 @@ public class cordovaPluginIngenico extends CordovaPlugin {
   /*
   private void processSaleRequest(RequestType.Sale saleReq,String ip_address,String port){
       TsiStatus ret = new TsiStatus();
-  
+
       System.out.println("iConnect-TSI version " + TransactionManager.getVersion());
       System.out.println("iConnect version " + Utility.iConnectVersion());
       IConnectDevice device = null;
-  
+
       try {
           device = new IConnectTcpDevice(ip_address, port);
           //pass "this" as an class implementing IConnectDevice.Logger
           // device.enableTsiTraces(true,this);
-  
+
           //By contract, first parameter is a sale request object
           RequestType.Sale req = saleReq;
-  
+
           TransactionManager transactionManager = new TransactionManager(device);
-  
+
           System.out.println("Connecting...  ");
           device.connect();
           System.out.println("Connected");
-  
+
           System.out.println("Sale request with amount of" + req.getAmount());
           System.out.println("Sending request ...  ");
-  
+
           transactionManager.sendRequest(req);
-  
+
           boolean multiTransaction = false;
           String refNo = null;
-  
-  
+
+
           do {
               System.out.println("Waiting for response...  ");
               ResponseType.Raw resp = transactionManager.receiveResponse();
-  
-  
+
+
               iConnectTsiTypes.TransactionStatus status = resp.getStatus();
               multiTransaction = resp.isMultiTransactionFlag();
-  
+
               //Handle receipt printing
               if (resp.getStatus().getTransactionStatus() == iConnectTsiTypes.TransactionStatus.ReceiptInformation) {
-  
+
                   multiTransaction = printReceipt(resp, transactionManager);
-  
+
               } else if (status.getTransactionStatus() == iConnectTsiTypes.TransactionStatus.Approved) {
                   ResponseType.Sale saleResp = new ResponseType.Sale().validate(resp);
-  
+
                   if (saleResp == null) {
                       System.out.println("ERROR: Cannot construct sale response object from raw. Reported type: " + resp.getType());
                       break;
                   } else {
                       System.out.println("Status: " + saleResp.getStatus());
-  
-  
+
+
                       System.out.println("Date: " + Integer.toString(saleResp.getTransactionDate()));
                       System.out.println("Time: " + Integer.toString(saleResp.getTransactionTime()));
                       System.out.println("Card Type: " + saleResp.getCustomerCardType());
@@ -294,9 +296,9 @@ public class cordovaPluginIngenico extends CordovaPlugin {
                   System.out.println("Cancelled or timed out");
                   break;
               }
-  
+
           } while (multiTransaction);
-  
+
           //Reference number is not null, which means we can use it to send a Void request
           if (refNo != null) {
               //reconnect to a terminal
@@ -305,20 +307,20 @@ public class cordovaPluginIngenico extends CordovaPlugin {
               //create and fill a void request object
               RequestType.VoidRequest voidReq = new RequestType.VoidRequest();
               voidReq.setReferenceNo(refNo);
-  
+
               System.out.println("Void request with reference number " + refNo + " is being sent...");
               transactionManager.sendRequest(voidReq);
-  
+
               do {
-  
+
                   ResponseType.Raw raw = transactionManager.receiveResponse();
                   iConnectTsiTypes.TransactionStatus status = raw.getStatus();
-  
+
                   //the same outcome as from using status.toString()
                   System.out.println("Status: " + Utility.TransactionStatusToString(status.getTransactionStatus()));
-  
+
                   multiTransaction = raw.isMultiTransactionFlag();
-  
+
                   if (status.getTransactionStatus() == iConnectTsiTypes.TransactionStatus.ReceiptInformation) {
                       multiTransaction = printReceipt(raw, transactionManager);
                   } else if (status.getTransactionStatus() == iConnectTsiTypes.TransactionStatus.Approved) {
@@ -331,18 +333,18 @@ public class cordovaPluginIngenico extends CordovaPlugin {
                   }
               } while (multiTransaction);
           }
-  
+
           System.out.println("Disconnecting ...  ");
           device.disconnect();
           System.out.println("OK");
-  
+
       } catch (TsiException e) {
           ret = new TsiStatus(e);
           System.out.println(e.getMessage());
       } finally {
           device.dispose();
       }
-  
+
   }
   */
 
